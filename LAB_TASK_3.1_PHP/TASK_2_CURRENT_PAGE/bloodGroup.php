13a6c7a76c8e354809618c90eb37c643f1521b26
DiptoMondol <diptomondol17@gmail.com>
2020-11-03 02:32:27 +0600

    form bloodGroup

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/bloodGroup.php
@@ -0,0 +1,31 @@
+<?php
+    if(isset($_REQUEST['submit'])){
+	   echo $_REQUEST['bloodGroup'];
+       }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Group</title>
+</head>
+<body>
+	<form method="post">
+		<fieldset>
+		<legend>Blood Group</legend>
+		<select name="bloodGroup">
+				    <option value="A+">A+</option>
+                    <option value="B+">B+</option>
+                    <option value="O+">O+</option>
+                    <option value="AB+">AB+</option>
+                    <option value="A-">A-</option>
+                    <option value="B-">B-</option>
+                    <option value="O-">O-</option>
+                    <option value="AB-">AB-</option>
+        </select>
+        <hr>
+        <input type="submit" name="submit" value="submit"> 
+        </fieldset>           
+	</form>
+
+</body>
+</html>
