0031ae089a28b0617d847a2c6f6a2a803714ef7d
DiptoMondol <diptomondol17@gmail.com>
2020-11-04 23:11:47 +0600

    BloodGroup Validated

---
+++ LAB_TASK_3.2_PHP/bloodGroupCheck.php
@@ -0,0 +1,15 @@
+<?php
+$blood =$_REQUEST['bloodGroup'];
+
+  if(empty($bloodGroup))
+  {
+      echo "Please Select your Blood Group";
+  }
+  else {
+      
+      echo "Submit Successful";
+  }
+
+?>
+
+
