e3c696019096f4a6ae9640f17f8eadbe12f10400
DiptoMondol <diptomondol17@gmail.com>
2020-11-05 02:19:07 +0600

    Email validated

--- LAB_TASK_3.2_PHP/emailCheck.php
+++ LAB_TASK_3.2_PHP/emailCheck.php
@@ -1,17 +1,21 @@
 <?php
-if(isset($_REQUEST['submit'])){
-	$email=$_REQUEST['email'];
-     
-     if(empty($email))
-        {
-			echo " Enter your email address!";
-		}
-	  elseif(!preg_match("/^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/",$email))
-	    {
-			echo "Invalid email address! "; 
-		}
-		 else {
-			echo $_POST['email'];  
-		}
+$emailErr ="";
+$email ="";
+if (empty($_POST["email"])) {
+  
+    echo "invalid email";
 }
+    else
+    {
+    	$email = $_POST["email"];
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $emailErr = "Invalid email format";
+		}
+	else
+	{
+		echo $email;
+	}
+
 ?>
