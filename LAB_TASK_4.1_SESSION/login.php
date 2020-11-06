a583f315739c0df7a8d38afa507b836f1c516541
DiptoMondol <diptomondol17@gmail.com>
2020-11-06 17:10:38 +0600

    updated

--- LAB_TASK_4.1_SESSION/login.php
+++ LAB_TASK_4.1_SESSION/login.php
@@ -1,6 +1,8 @@
 <?php
 
-	
+session_start();
+if(isset($_COOKIE['checkRemember']))
+{	
 ?>
 
 <!DOCTYPE html>
@@ -9,24 +11,24 @@
 	<title>Login</title>
 </head>
 <body>
-	<form  method="POST" action="logincheck.php">
+	<form action="logincheck.php" method="POST">
 		<fieldset>
-			<legend><b>LOGIN</b></legend>
+			<legend>LOGNIN</legend>
 		<table>
 			<tr>
 				<td>User Name :</td>
-				<td><input type="text" name="username" ></td>
+				<td><input type="text" name="username" value ="<?php echo $_COOKIE['uname']; ?>"></td>
 			</tr>
 			<tr>
 				<td>Password :</td>
-				<td><input type="password" name="password""></td>
+				<td><input type="password" name="password" value ="<?php echo $_COOKIE['password']; ?>"></td>
 			
 			</tr>
 		
 		</table>
 		
 		<hr>
-			<input type="checkbox" name="checkRemember" >Remember Me
+			<input type="checkbox" name="checkRemember" autocomplete="on" checked>Remember Me
 			<br><br>
 			<input type="submit" name="submit" value="Submit"> <a href="forgotpassword.html">Forgot Paswword?</a>
 			
@@ -36,3 +38,42 @@
 </body>
 </html>
                 
+<?php
+}
+else{
+	?>
+	<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<form action="logincheck.php" method="POST">
+		<fieldset>
+			<legend>SIGNIN</legend>
+		<table>
+			<tr>
+				<td>User Name :</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+                <td><input type="password" name="password"></td>
+               
+            </tr>
+           
+		</table>
+           
+        <hr>
+              <input type="checkbox" name="checkRemember">Remember Me
+               <br><br>
+              <input type="submit" name="submit" value="Submit"> <a href="forgotpassword.html">Forgot Paswword?</a>
+			
+	
+		</fieldset>
+	</form>
+</body>
+</html>
+<?php
+}
+?>
--- LAB_TASK_4.1_SESSION/publicHome.php
+++ LAB_TASK_4.1_SESSION/publicHome.php
@@ -1,16 +1,49 @@
 <?php
+	//session_start();
 	
-?>
+	//if($_SESSION['status'] == "Ok"){ 
+    
+        if(isset($_REQUEST['hbutton']))
+        {
+            header('location: publicHome.php');
+        }
+        elseif(isset($_REQUEST['lbutton']))
+        {
+            header('location: login.html');
+        }
+        elseif(isset($_REQUEST['rbutton']))
+        {
+            header('location: Registration.html');
+        }
+        
 
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Public Home</title>
+    <title>Document</title>
 </head>
 <body>
-	<h1>Welcome to xCompany! </h1>
-	<a href="logout.php">logout</a>
+    <form action="#" method="POST">
+        <table border="1" >
+            <tr>
+                <td>Xcompany
+                <a href = "publicHome.php" name="hbutton">Home</a>|
+                <a href = "login.php" name="lbutton">Login</a>|
+                <a href = "registration.html"  name="rbutton">Registration</a>
+            </td>
+    
+            </tr>
+            <tr align="center">
+                <td>Welcome to xCompany</td>
+            </tr>
+            <tr align="center">
+                <td>Copyright@ 2017</td>
+            </tr>
+        </table>
+    </form>
+    
+
 </body>
 </html>
-
