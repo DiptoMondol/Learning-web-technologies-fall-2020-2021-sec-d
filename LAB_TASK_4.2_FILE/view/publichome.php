8f58aa846bfc8b232358aa04d10ab06e0910d9e9
DiptoMondol <diptomondol17@gmail.com>
2020-11-10 01:02:31 +0600

    view

---
+++ LAB_TASK_4.2_FILE/view/publichome.php
@@ -0,0 +1,39 @@
+<?php
+    require_once('../php/header.php');
+?>
+
+<html>
+<head>
+    <title>PUBLIC HOME</title>
+</head>
+<body>
+
+    <table  border="1" align="center">
+        <tr colspan="3">
+            <td>
+                <img src="../assets/picture.png" width="100px" height="100px" ></td>
+                <td align="right">
+                <a href="../view/public_home.php"><b>Home</b></a> | 
+                <a href="../view/login.php"><b>Login</b></a> | 
+                <a href="../view/registration.php"><b>Registration</b></a> 
+            </td>
+        </tr>
+        <tr >
+            <td colspan="3" align="left">
+                <b>Welcome to my Company</b>
+                
+            </td>
+        </tr>
+                
+        <tr height="50px">
+            <td colspan="3" align="center">
+                Copyright@2017
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
+<?php
+    require_once('../php/header.php');
+?>
