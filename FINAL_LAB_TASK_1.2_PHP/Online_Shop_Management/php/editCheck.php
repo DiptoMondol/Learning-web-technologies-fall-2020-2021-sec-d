7533b4be5efd0d49d0169a0ce32d1f9e780eb6c7
DiptoMondol <diptomondol17@gmail.com>
2020-12-02 09:36:28 +0600

    final commit

--- FINAL_LAB_TASK_1.2_PHP/Online_Shop_Management/php/editCheck.php
+++ FINAL_LAB_TASK_1.2_PHP/Online_Shop_Management/php/editCheck.php
@@ -1,3 +1,15 @@
 <?php
-updateReaderInfo();
-?>
+
+        require_once('../model/userService.php');
+		if(isset($_POST['submit'])){
+		if(isset($_POST['name']) || isset($_POST['cno']) || isset( $_POST['username']) || isset($_POST['password'])){
+			
+		$name = $_POST['name'];
+		$cno = $_POST['cno'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$conn = getConnection();
+		$sql = $conn->updateReaderInfo($conn, $name, $cno,$username,$password);
+		}
+		
+		}
