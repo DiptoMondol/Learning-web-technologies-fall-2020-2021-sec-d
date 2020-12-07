3d2587889693e8242be03f2ec4817724ed01fe91
DiptoMondol <diptomondol17@gmail.com>
2020-12-07 10:14:36 +0600

    updated

--- FINAL_LAB_TASK_JS_3.1/script.js
+++ FINAL_LAB_TASK_JS_3.1/script.js
@@ -12,12 +12,29 @@
 
 let student = ['alamin', '123', 'CS'];
 
-function f1(){
-	let data= document.getElementById('name').value;
-	if(data == ''){
-		document.getElementById('head').innerHTML = 'invalid data';		
-	}else{
-		document.getElementById('head').innerHTML = data;
+function f1()
+{
+	var name = document.getElementById("Name").value;
+
+   if (name == "")
+   {  
+      alert("Please enter name first");
+      return false;
+   }
+   else if((name.charAt(0) <= 'a' || name.charAt(0) >= 'z') && (name.charAt(0) <= 'A' || name.charAt(0) >= 'Z'))
+	{
+		alert("Name must start with a letter");
+		return false;
+	}
+	else if(name.split(" ").length<2)
+	{
+		alert("Name must contain at least two words");
+		return false;
+	}
+	else
+	{ 
+		return true;
 	}
 
+}
 }
